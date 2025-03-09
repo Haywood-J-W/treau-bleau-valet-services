a74706ba5b0b22ce0925c5e3c3bde0e829a1b149
Haywood J.W. <haywoodjwhite@gmail.com>
2025-03-09 01:32:19 -0500

    Initial Next.js project commit

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,17 @@
+
+// Home Page for Treau Bleau Valet Services
+
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Treau Bleau Valet Services</title>
+        <meta name="description" content="Taking Out the Trash, So You Don’t Have To." />
+      </Head>
+      <h1>Welcome to Treau Bleau Valet Services</h1>
+      <p>Your premier valet trash service for apartments & condos.</p>
+    </div>
+  );
+}
